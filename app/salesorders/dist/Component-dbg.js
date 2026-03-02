72c36ca37bdec0e8b9c0c16e6e6e7a1dc3c428c3
Dimo Uzunov <dimo.uzunov@westernacher.com>
2026-03-02 18:22:58 +0100

    fix: stabilize HTML5 app content deployment for Work Zone

---
+++ app/salesorders/dist/Component-dbg.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "sap/fe/core/AppComponent"
+], function (Component) {
+    "use strict";
+
+    return Component.extend("salesorders.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
